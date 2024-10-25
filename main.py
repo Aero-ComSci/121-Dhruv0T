64c529af66af936811c73031042f69bbf5896dcd
Dhruv Torney <148287814+Dhruv0T@users.noreply.github.com>
2024-10-24 23:17:02 -0700

    Update main.py
    started it with a partner but will redo by myself tomorrow in class

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 
+import turtle as trtl
+import random
+
+# Global variables
+score = 0
+time_left = 10  # You can adjust the total time here
+
+
+def start_game():
+    global score, time_left
+
+    # Create the turtle that will be clicked
+    turt = trtl.Turtle()
+
+    # Turtles for displaying score and time
+    scoreKeeper = trtl.Turtle()
+    timeKeeper = trtl.Turtle()
+    scoreKeeper.hideturtle()
+    timeKeeper.hideturtle()
+
+    # Move score/time turtles to positions
+    teleportation(scoreKeeper, [0, 370])
+    teleportation(timeKeeper, [0, 340])
+
+    # Initial display of score and time
+    font_setup = ("Arial", 20, "normal")
+    text_update(scoreKeeper, score, "Your score: ", font_setup)
+    text_update(timeKeeper, time_left, "Time left: ", font_setup)
+
+    # Set up the game window
+    wind = trtl.Screen()
+    wind.screensize(800, 800)
+    wind.title("Catch-A-Turtle Game")
+    wind.bgcolor("lightblue")
